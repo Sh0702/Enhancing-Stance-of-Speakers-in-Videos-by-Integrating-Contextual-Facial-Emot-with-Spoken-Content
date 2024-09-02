5f2362cef1544325a1f8de0d3eb784140ef46926
Shreyas Srinivasan <53533928+Sh0702@users.noreply.github.com>
2024-09-02 11:15:54 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from face_tracking_factory import FaceTrackingFactory
+
+# Path to your video file
+video_path = '/path/to/video'
+
+# Path to the output video file
+output_path = '/path/to/output'
+
+# Path to the emotions file
+emotions_file_path = '/path/to/csv_file'
+
+# Create an instance of FaceTrackingFactory
+face_tracker = FaceTrackingFactory()
+
+# Process the video
+face_tracker.process_video(video_path, output_path, emotions_file_path)
